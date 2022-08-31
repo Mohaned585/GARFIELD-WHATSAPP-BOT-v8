fc9cd6b1dc89044a363d6fe96615559a634f5d1e
Mohaned585 <112529448+Mohaned585@users.noreply.github.com>
2022-08-31 04:14:04 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -16,28 +16,28 @@ global.APIKeys = {
 global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
 global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.notice = ` *Hi 🥰 I m*
-*ＮＯＩＺ Ξ*
+*R3d Ξ*
 *I am 16 years old developer...*
 *From Sri Lanka 🇱🇰*
 *I am a Learning.*
 *Not more..*
 *Also I am Student*
-*Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
-global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ' //👈 Enter Your Bot name
-global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
+*خد واتس - https://wa.me/+201280063001*`  // 👈 Enter Your details and Social Media Links and Anything
+global.owner =  ['+201280063001'] //👈  Enter Your number
+global.premium =  ['+20180063001'] //👈  Enter Your number
+global.ownernomer = '+201280063001' //👈  Enter Your number
+global.ownername = 'R3D' //👈 Enter Your name
+global.botname = 'R3D ＢＯＴ' //👈 Enter Your Bot name
+global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 R3D 𝖡𝗈𝗍' //👈 Enter Your Name
 global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
 global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
 global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
 global.fbt = 'Follw Us ℹ️' // 👈 Enter (Subscribe/Follow/Like now)
-global.myweb = 'https://www.facebook.com/garfieldbots/'// 👈 Enter your Social media link to follow now button
-global.welcome = '*Welcome To The Group 🦋*' // 👈 You Can change this your choice
-global.bye = '*Bye bye Mother fucker 🐼*' // 👈 You Can change this your choice
-global.packname = 'GARFIELD-BOTv8'  // 👈 You Can change this your choice 
-global.author = 'NOIZE DEVELOPMENT' // 👈 You Can change this your choice 
+global.myweb = 'https://wa.me/+201280063001'// 👈 Enter your Social media link to follow now button
+global.welcome = '*💗نورت الروم🦋*' // 👈 You Can change this your choice
+global.bye = '*الروم خسر معزه🐼*' // 👈 You Can change this your choice
+global.packname = 'R3D-BOTv8'  // 👈 You Can change this your choice 
+global.author = 'R3D DEVELOPMENT' // 👈 You Can change this your choice 
 global.sessionName = 'session' // 👈 You Can change this your choice
 global.prefa = ['','!','.','🛰️','⚓','🍎'] // 👈 You Can change this your choice
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
@@ -47,16 +47,15 @@ global.spoty = 'https://i.postimg.cc/HW2YbN1b/Pics-Art-22-08-09-16-12-14-489.png
 global.sp = '⭔'
 global.mess = {
     success: 'Done 🦋', // 👈 You Can change this your choice
-    admin: 'This Feature Is Only For Admin!', // 👈 You Can change this your choice
-    botAdmin: 'Bot Must Be Admin First!', // 👈 You Can change this your choice
-    owner: 'This Feature Is Only For Owner!', // 👈 You Can change this your choice
-    group: 'Feature Used Only For Groups!', // 👈 You Can change this your choice
-    private: 'Features Used Only For Private Chat!', // 👈 You Can change this your choice
-    bot: 'This Feature Is Only For Bot', // 👈 You Can change this your choice
-    wait: '```Please Wait...```', // 👈 You Can change this your choice
+    admin: 'بطل لعب يا بابا, // 👈 You Can change this your choice
+    botAdmin: 'لازم اكون ادمن!', // 👈 You Can change this your choice
+    owner: 'لما رعد يؤمرني!', // 👈 You Can change this your choice
+    group: 'في الجروبات بس!', // 👈 You Can change this your choice
+    private: 'في البرايفيت بس!', // 👈 You Can change this your choice
+    bot: 'دا شغلي انا ملكش دعوه', // 👈 You Can change this your choice
+    wait: 'اصبر علي نفسك الدنيا هتطير', // 👈 You Can change this your choice
     error: 'Error! Maybe Api Key Is Expired🤔!', // 👈 You Can change this your choice
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-} // 👈 You Can change this your choice
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours', } // 👈 You Can change this your choice
 /// Do Not Change 👇 
 global.limitAwal = "99999999999999999"
 global.lolkey = "835641f3d2d1b6971e1fd132"
